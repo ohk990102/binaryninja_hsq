02bc7e33b2b3c7c6c1410d9ae45c3e26533fcf72
ohk990102 <ohk990102@naver.com>
2021-03-13 15:04:35 +0900

    initial commit

---
+++ __init__.py
@@ -0,0 +1,229 @@
+from platform import architecture
+from binaryninja.binaryview import BinaryView
+from binaryninja.architecture import Architecture
+from binaryninja.callingconvention import CallingConvention
+from binaryninja.enums import BranchType, InstructionTextTokenType, SectionSemantics, SegmentFlag
+from binaryninja.function import InstructionInfo, InstructionTextToken, RegisterInfo
+from binaryninja.log import log_info
+import struct
+
+from binaryninja.lowlevelil import LowLevelILFunction, LowLevelILLabel
+
+from subleq64.instructions import *
+
+if __name__ == "__main__":
+    from instructions import *
+
+
+class HigherSubleq64(Architecture):
+    name = 'hsq64'
+    address_size = 8
+    default_int_size = 8
+    instr_alignment = 1
+    max_instr_length = address_size * 32
+    disassembler: HsqDisassembler = None
+
+    regs = {
+        'sp': RegisterInfo('sp', 8),
+        'bp': RegisterInfo('bp', 8),
+        'ax': RegisterInfo('ax', 8)
+    }
+    stack_pointer = 'sp'
+
+    def get_instruction_info(self, data, addr):
+        instr = self.disassembler.instrs[addr // self.address_size]
+        if instr is None:
+            return None
+
+
+        result = InstructionInfo()
+        result.length = instr.width * self.address_size
+        next_addr = instr.get_next_addr()
+        if isinstance(instr, Call):
+            result.add_branch(BranchType.CallDestination, instr.c * self.address_size)
+        elif isinstance(instr, Ret) or isinstance(instr, Exit):
+            result.add_branch(BranchType.FunctionReturn)
+        else:
+            if len(next_addr) == 2:
+                result.add_branch(BranchType.TrueBranch, next_addr[1] * self.address_size)
+                result.add_branch(BranchType.FalseBranch, next_addr[0] * self.address_size)
+            elif len(next_addr) == 1:
+                result.add_branch(BranchType.UnconditionalBranch, next_addr[0] * self.address_size)
+        return result
+        
+    
+    def get_instruction_text(self, data, addr):
+        instr = self.disassembler.instrs[addr // self.address_size]
+        if instr is None:
+            return None
+        
+        tokens = []
+        tokens.append(InstructionTextToken(InstructionTextTokenType.TextToken, type(instr).__name__.lower()))
+        tokens.append(InstructionTextToken(InstructionTextTokenType.TextToken, ' '))
+        for i in range(len(instr.operands)):
+            operand = instr.operands[i]
+            if self.disassembler.is_register(operand):
+                tokens.append(InstructionTextToken(InstructionTextTokenType.RegisterToken, self.disassembler.symbol[operand]))
+            else:
+                tokens.append(InstructionTextToken(InstructionTextTokenType.PossibleAddressToken, hex(operand * self.address_size)))
+
+            if i != len(instr.operands) - 1:
+                tokens.append(InstructionTextToken(InstructionTextTokenType.OperandSeparatorToken, ', '))
+
+        return tokens, instr.width * self.address_size
+    
+    def get_instruction_low_level_il(self, data, addr, il: LowLevelILFunction):
+        instr = self.disassembler.instrs[addr // self.address_size]
+        if instr is None:
+            return None
+
+        if isinstance(instr, Subleq):
+            a, b, c = instr.a, instr.b, instr.c
+
+            _, mem_a = self.get_addr_mem_il(a, il)
+            addr_b, mem_b = self.get_addr_mem_il(b, il)
+            sub_op = il.sub(self.address_size, mem_b, mem_a)
+            if self.disassembler.is_register(b):
+                store_b = il.set_reg(self.address_size, self.disassembler.symbol[b], sub_op)
+            else:
+                store_b = il.store(self.address_size, addr_b, sub_op)
+            il.append(store_b)
+            less_op = il.compare_signed_less_equal(self.address_size, mem_b, il.const(self.address_size, 0))
+            
+            t_target = il.get_label_for_address(il.arch, c * self.address_size)
+            t_label_found = True
+            if t_target is None:
+                t_label_found = False
+                t_target = LowLevelILLabel()
+            
+            f_label_found = True
+            f_target = il.get_label_for_address(il.arch, addr + instr.width + self.address_size)
+            if f_target is None:
+                f_target = LowLevelILLabel()
+                f_label_found = False
+
+            il.append(il.if_expr(less_op, t_target, f_target))
+
+            if not t_label_found:
+                il.mark_label(t_target)
+                il.append(il.jump(il.const(self.address_size, c * self.address_size)))
+            if not f_label_found:
+                il.mark_label(f_target)
+        elif isinstance(instr, Clear):
+            b = instr.b
+            c = instr.c
+
+            addr_b, _ = self.get_addr_mem_il(b, il)
+            store_b = il.store(self.address_size, addr_b, il.const(self.address_size, 0))
+            il.append(store_b)
+            jump_c = il.jump(il.const(self.address_size, instr.c * self.address_size))
+            il.append(jump_c)
+
+            
+        elif isinstance(instr, Push):
+            v = instr.v
+
+            addr_v, mem_v = self.get_addr_mem_il(v, il)
+            push_v = il.push(self.address_size, mem_v)
+
+            il.append(push_v)
+        elif isinstance(instr, Mov):
+            a, b = instr.a, instr.b
+
+            addr_a, mem_a = self.get_addr_mem_il(a, il)
+            addr_b, mem_b = self.get_addr_mem_il(b, il)
+            if self.disassembler.is_register(b):
+                mov_op = il.set_reg(self.address_size, self.disassembler.symbol[b], mem_a)
+            else:
+                mov_op = il.store(self.address_size, addr_b, mem_a)
+            il.append(mov_op)
+        elif isinstance(instr, Ret):
+            il.append(il.ret(il.load(self.address_size, il.reg(self.address_size, 'sp'))))
+        elif isinstance(instr, Pop):
+            v = instr.v
+            addr_v, _ = self.get_addr_mem_il(v, il)
+            pop_op = il.pop(self.address_size)
+            if self.disassembler.is_register(v):
+                store_op = il.set_reg(self.address_size, self.disassembler.symbol[v], pop_op)
+            else:
+                store_op = il.store(self.address_size, addr_v, pop_op)
+            il.append(store_op)
+        elif isinstance(instr, Call):
+            il.append(il.call(il.const(self.address_size, instr.c * self.address_size)))
+        elif isinstance(instr, Inc):
+            b = instr.b
+            addr_b, mem_b = self.get_addr_mem_il(b, il)
+            if self.disassembler.is_register(b):
+                store_op = il.set_reg(self.address_size, self.disassembler.symbol[b], il.add(self.address_size, mem_b, il.const(self.address_size, 1)))
+            else:
+                store_op = il.store(self.address_size, addr_b, il.add(self.address_size, mem_b, il.const(self.address_size, 1)))
+            il.append(store_op)
+        elif isinstance(instr, Dec):
+            b = instr.b
+            addr_b, mem_b = self.get_addr_mem_il(b, il)
+            if self.disassembler.is_register(b):
+                store_op = il.set_reg(self.address_size, self.disassembler.symbol[b], il.add(self.address_size, mem_b, il.const(self.address_size, -1)))
+            else:
+                store_op = il.store(self.address_size, addr_b, il.add(self.address_size, mem_b, il.const(self.address_size, -1)))
+            il.append(store_op)
+        elif isinstance(instr, Exit):
+            il.append(il.no_ret())
+        elif isinstance(instr, Jmp):
+            il.append(il.jump(il.const(self.address_size, instr.c * self.address_size)))
+
+        return instr.width * self.address_size
+
+    def get_addr_mem_il(self, addr, il):
+        if self.disassembler.is_register(addr):
+            addr_il = il.reg(self.address_size, self.disassembler.symbol[addr])
+            mem_il = addr_il
+        else:
+            addr_il = il.const_pointer(self.address_size, addr * self.address_size)
+            mem_il = il.load(self.address_size, addr_il)
+        return addr_il, mem_il
+
+HigherSubleq64.register()
+
+class DefaultCallingConvention(CallingConvention):
+    name = 'default'
+    int_arg_regs = []
+    int_return_reg = 'ax'
+
+arch = Architecture['hsq64']
+arch.register_calling_convention(DefaultCallingConvention(arch, 'default'))
+arch.standalone_platform.default_calling_convention = arch.calling_conventions['default']
+
+class HigherSubleqView(BinaryView):
+    name = 'hsq'
+    long_name = 'Higher Subleq Binary'
+
+    @classmethod
+    def is_valid_for_data(self, data):
+        header = data.read(0,8)
+        return header == b'\x00'*8
+
+    def __init__(self, data):
+        BinaryView.__init__(self, parent_view = data, file_metadata=data.file)
+
+        # TODO: allow different bits
+        self.data = data
+
+        HigherSubleq64.disassembler = HsqDisassembler(self.data[:], 8)
+        self.arch = Architecture['hsq64']
+        self.platform = Architecture['hsq64'].standalone_platform
+
+    def init(self):
+        self.add_auto_segment(
+            0, len(self.data),
+            0, len(self.data),
+            SegmentFlag.SegmentReadable | SegmentFlag.SegmentWritable | SegmentFlag.SegmentExecutable 
+        )
+        return True
+
+    def perform_is_executable(self):
+        return True
+    
+    def perform_get_entry_point(self):
+        return 0
+
+HigherSubleqView.register()
---
+++ instructions.py
@@ -0,0 +1,356 @@
+import abc
+from collections import OrderedDict
+import struct
+from typing import List, Optional, Union
+
+class Instr(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, addr: int, data: List[int]):
+        self.addr = addr
+        self.data = data
+        self.width = len(data)
+        self.operands = []
+
+    def get_next_addr(self) -> List[int]:
+        if self.c not in [-1, 0, self.addr + self.width]:
+            return [self.addr + self.width, self.c]
+        return [self.addr + self.width]
+        
+    def __str__(self):
+        ret = f'{type(self).__name__.lower()}'
+        for operand in self.operands:
+            ret += f' {operand}'
+        return ret
+
+    def get_human_friendly_str(self, registers):
+        ret = f'{type(self).__name__.lower()}'
+        for operand in self.operands:
+            if operand in registers.keys():
+                ret += f' {registers[operand]}'
+            else:
+                ret += f' {operand}'
+        return ret
+
+
+class Subleq(Instr):
+    def __init__(self, addr: int, data: List[int]):
+        super().__init__(addr, data)
+        self.a, self.b, self.c = self.data
+        self.operands = [self.a, self.b, self.c]
+
+class Clear(Instr):
+    def __init__(self, addr: int, data: List[int]):
+        super().__init__(addr, data)
+        self.a, self.b, self.c = self.data
+        self.operands = [self.a, self.c]
+
+    def get_next_addr(self) -> List[int]:
+        if self.c not in [-1, 0]:
+            return [self.c]
+        return []
+
+class Exit(Instr):
+    def get_next_addr(self) -> List[int]:
+        return []
+
+class Jmp(Instr):
+    def __init__(self, addr: int, data: List[int]):
+        super().__init__(addr, data)
+        self.c = self.data[2]
+        self.operands = [self.c]
+
+    def get_next_addr(self) -> List[int]:
+        return [self.c]
+
+class Mov(Instr):
+    def __init__(self, addr: int, data: List[int]):
+        super().__init__(addr, data)
+        self.a = self.data[3]
+        self.b = self.data[4]
+        self.c = self.data[5]
+        self.operands = [self.a, self.b, self.c]
+
+class Dec(Instr):
+    def __init__(self, addr: int, data: List[int]):
+        super().__init__(addr, data)
+        self.a, self.b, self.c = self.data
+        self.operands = [self.b, self.c]
+
+    def get_next_addr(self) -> List[int]:
+        if self.c not in [-1, 0, self.addr + self.width]:
+            return [self.addr + self.width, self.c]
+        return [self.addr + self.width]
+
+class Inc(Instr):
+    def __init__(self, addr: int, data: List[int]):
+        super().__init__(addr, data)
+        self.b = self.data[1]
+        self.operands = [self.b]
+
+    def get_next_addr(self) -> List[int]:
+        return [self.addr + self.width]
+
+class Call(Instr):
+    def __init__(self, addr: int, data: List[int]):
+        super().__init__(addr, data)
+        self.c = data[26]
+        self.operands = [self.c]
+
+    def get_next_addr(self) -> List[int]:
+        if self.c not in [-1, 0]:
+            return [self.addr + self.width, self.c]
+        return [self.addr + self.width]
+
+class Ret(Instr):
+    def get_next_addr(self) -> List[int]:
+        return []
+
+class Push(Instr):
+    def __init__(self, addr: int, data: List[int]):
+        super().__init__(addr, data)
+        self.v = data[24]
+        self.c = data[26]
+        self.operands = [self.v, self.c]
+
+    def get_next_addr(self) -> List[int]:
+        if self.c not in [-1, 0, self.addr + self.width]:
+            return [self.addr + self.width, self.c]
+        return [self.addr + self.width]
+
+class Pop(Instr):
+    def __init__(self, addr: int, data: List[int]):
+        super().__init__(addr, data)
+        self.v = data[10]
+        self.c = data[14]
+        self.operands = [self.v, self.c]
+
+    def get_next_addr(self) -> List[int]:
+        if self.c not in [-1, 0, self.addr + self.width]:
+            return [self.addr + self.width, self.c]
+        return [self.addr + self.width]
+
+rules = OrderedDict()
+rules[Clear] = {
+    'pattern': [
+        (Subleq, 'A', 'A', None)
+    ],
+    'width': 3
+}
+rules[Jmp] = {
+    'pattern': [
+        (Clear, 'Z', None)
+    ],
+    'width': 3
+}
+rules[Exit] = {
+    'pattern': [
+        (Jmp, -1)
+    ],
+    'width': 3
+}
+rules[Dec] = {
+    'pattern': [
+        (Subleq, 'dec', None, None)
+    ],
+    'width': 3
+}
+rules[Inc] = {
+    'pattern': [
+        (Subleq, 'inc', None, None)
+    ],
+    'width': 3
+}
+rules[Mov] = {
+    'pattern': [
+        (Clear, 'A', '$'), (Subleq, None, 'A', None)
+    ],
+    'width': 6,
+}
+rules[Push] = {
+    'pattern': [
+        (Dec, 'sp', '$'), (Mov, 'sp', '$+6', '$'), (Mov, 'sp', '$+1', '$'),
+        (Clear, 0, '$'), (Mov, 'sp', '$+1', '$'), (Subleq, None, 0, None)
+    ],
+    'width': 27,
+}
+rules[Pop] = {
+    'pattern': [
+        (Mov, 'sp', '$+3', '$'), (Mov, 0, None, '$'), (Inc, 'sp', '$')
+    ],
+    'width': 15,
+}
+rules[Call] = {
+    'pattern': [
+        (Push, '$', None), 
+    ],
+    'width': 28,
+}
+rules[Ret] = {
+    'pattern': [
+        (Mov, 'sp', '$+3', '$'), (Mov, 0, '$+2', '$'), (Jmp, 0)
+    ],
+    'width': 15,
+}
+
+keywords = [
+    'inc', 'Z', 'dec', 'ax', 'bp', 'sp'
+]
+
+class HsqDisassembler(object):
+    def __init__(self, data: bytes, int_size: int):
+        self.int_size = int_size
+        if self.int_size not in (4, 8):
+            raise Exception('Invalid int size. ')
+        self.instr_align = 3
+        self.data = list(map(lambda v: v[0], struct.iter_unpack('<i' if self.int_size == 4 else '<q', data)))
+        self.instrs: list[Optional[Instr]] = [ None for _ in range(len(self.data)) ]
+        self.tabs = -1
+        self._phase1()
+        self._phase2()
+        
+
+    def get_instr(self, addr: int):
+        if self.is_valid_addr(addr):
+            return self.instrs[addr]
+        return None
+        
+    def put_instr(self, addr: int, instr: Instr):
+        self.instrs[addr] = instr
+        for i in range(addr+1, addr + instr.width):
+            self.instrs[i] = None
+
+    def gen_simple_instr(self, addr: int):
+        if not self.is_valid_addr(addr, 3):
+            return None
+
+        data = self.data[addr:addr+3]
+        instr = Subleq(addr, data)
+        return instr
+
+    def _phase1(self):
+        # Heuristics on getting register info
+        top = self.gen_simple_instr(0)
+        if not (top.a == 0 and top.b == 0):
+            raise Exception('Does not match higher subleq arch')
+        self.sqmain = top.get_next_addr()[1]
+        instr: Subleq = self.gen_simple_instr(self.sqmain)
+        self.inc = instr.a - 2
+        self.Z = instr.a -1
+        self.dec = instr.a
+        self.ax = instr.a + 1
+        self.bp = instr.a + 2
+        self.sp = instr.a + 3
+        self.symbol = {
+            self.inc: 'inc',
+            self.Z: 'Z',
+            self.dec: 'dec',
+            self.ax: 'ax',
+            self.bp: 'bp',
+            self.sp: 'sp'
+        }
+
+    def _phase2(self):
+        visited = set()
+        queue = set([0])
+
+        while len(queue) != 0:
+            addr = queue.pop()
+            if addr in visited:
+                continue
+            visited.add(addr)
+            instr = self.put_best_instr(addr)
+            if instr is not None:
+                for addr in instr.get_next_addr():
+                    queue.add(addr)
+
+    def put_best_instr(self, addr: int) -> Optional[Instr]:
+        instr = self.get_instr(addr)
+        if instr is not None:
+            return instr
+        instr = self.gen_simple_instr(addr)
+        if instr is None:
+            return None
+        self.tabs += 1
+        print('\t'*self.tabs + f'{addr}: [None] -> {str(instr)}')
+        
+        self.put_instr(addr, instr)
+        for key, rule in rules.items():
+            if self.peek_and_test_pattern(addr, rule['pattern']):
+                print('\t'*self.tabs + f'{addr}: {str(instr)} -> ', end='')
+                instr = key(addr, self.data[addr:addr+rule['width']])
+                print(f'{str(instr)}')
+                self.put_instr(addr, instr)
+        self.tabs -= 1
+        return instr
+
+    def peek_and_test_pattern(self, addr: int, pattern: List[tuple]) -> bool:
+        cu = addr
+        cache = {}
+        for i in range(len(pattern)):
+            part = pattern[i]
+            instr = self.get_instr(cu)
+            if instr is None:
+                instr = self.put_best_instr(cu)
+                if instr is None:
+                    return False
+            
+            if not isinstance(instr, part[0]):
+                return False
+            for j in range(len(instr.operands)):
+                if part[j+1] is None:
+                    continue
+                elif isinstance(part[j+1], int):
+                    if part[j+1] != instr.operands[j]:
+                        return False
+                elif part[j+1].startswith('$'):
+                    offset = 0 if part[j+1][1:] == '' else int(part[j+1][1:])
+                        
+                    if cu + instr.width + offset != instr.operands[j]:
+                        return False
+                elif part[j+1] in keywords:
+                    if getattr(self, part[j+1]) != instr.operands[j]:
+                        return False
+                else:
+                    if part[j+1] in cache.keys():
+                        if cache[part[j+1]] != instr.operands[j]:
+                            return False
+                    else:
+                        cache[part[j+1]] = instr.operands[j]
+            next = instr.get_next_addr()
+            if i == len(pattern) - 1:
+                break
+            if len(next) != 1:
+                return False
+            cu = next[0]
+        return True
+            
+
+    def is_valid_addr(self, addr: int, width: int = 1) -> bool:
+        return 0 <= addr < len(self.data) - (width - 1)
+
+    def is_register(self, addr: int) -> bool:
+        return addr in self.symbol.keys() and self.symbol[addr] in ['sp', 'bp', 'ax']
+
+    def is_fixed_symbol(self, addr: int) -> bool:
+        return addr in self.symbol.keys() and self.symbol[addr] in ['inc', 'Z', 'dec']
+
+    def __str__(self):
+        ret = ''
+        for i in range(len(self.instrs)):
+            if self.instrs[i] is not None:
+                ret += f'{i}: {self.instrs[i].get_human_friendly_str(self.symbol)}\n'
+        return ret
+
+if __name__ == "__main__":
+    with open('./test2.bin', 'rb') as f:
+        data = f.read()
+    disassembler = HsqDisassembler(data, 8)
+    # print(disassembler.data)
+    print(disassembler)
+    print(disassembler.data[disassembler.inc])
+    print(disassembler.data[disassembler.Z])
+    print(disassembler.data[disassembler.dec])
+    print(disassembler.data[disassembler.ax])
+    print(disassembler.data[disassembler.bp])
+    print(disassembler.data[disassembler.sp])
